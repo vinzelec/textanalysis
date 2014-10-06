ba45d2f3d66dc431ccb650643a17421cecda9b4e
vinzelec <leclercq.vincent@gmail.com>
2014-10-06 22:18:09 +0200

    providing implementation for SegmentedTextDocument

---
+++ src/main/java/fr/vinze/textanalysis/document/impl/SegmentedTextDocumentImpl.java
@@ -0,0 +1,43 @@
+package fr.vinze.textanalysis.document.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.vinze.textanalysis.document.RawTextDocument;
+import fr.vinze.textanalysis.document.SegmentedTextDocument;
+import fr.vinze.textanalysis.document.Token;
+
+public class SegmentedTextDocumentImpl implements SegmentedTextDocument {
+
+	final RawTextDocument source;
+	String name;
+	List<Token> tokens;
+
+	public SegmentedTextDocumentImpl(RawTextDocument source) {
+		this(source.getName(), source);
+	}
+
+	public SegmentedTextDocumentImpl(String name, RawTextDocument source) {
+		super();
+		this.source = source;
+		this.name = name;
+		tokens = new ArrayList<Token>();
+	}
+
+	public RawTextDocument getSource() {
+		return source;
+	}
+
+	public List<Token> getTokens() {
+		return tokens;
+	}
+
+	public void setTokens(List<Token> tokens) {
+		this.tokens = tokens;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
