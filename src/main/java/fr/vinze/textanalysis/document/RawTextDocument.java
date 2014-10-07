744b32f2ca8cd97e510a629a2b9a45ec7f0f994c
vinzelec <leclercq.vincent@gmail.com>
2014-10-07 16:20:01 +0200

    added raw source to raw text documents

--- src/main/java/fr/vinze/textanalysis/document/RawTextDocument.java
+++ src/main/java/fr/vinze/textanalysis/document/RawTextDocument.java
@@ -4,6 +4,15 @@ public interface RawTextDocument {
 
 	String getName();
 
+	/**
+	 * @return the content of the source document (with tags for markup file for
+	 *         example)
+	 */
+	String getRawSource();
+
+	/**
+	 * @return the content as raw text (and only the text)
+	 */
 	String getContent();
 		
 }
--- src/main/java/fr/vinze/textanalysis/document/impl/RawTextDocumentImpl.java
+++ src/main/java/fr/vinze/textanalysis/document/impl/RawTextDocumentImpl.java
@@ -4,12 +4,20 @@ import fr.vinze.textanalysis.document.RawTextDocument;
 
 public class RawTextDocumentImpl implements RawTextDocument {
 
-	final String name, content;
+	final String name, content, rawSource;
 	
 	public RawTextDocumentImpl(String name, String content) {
 		super();
 		this.name = name;
 		this.content = content;
+		this.rawSource = content;
+	}
+
+	public RawTextDocumentImpl(String name, String content, String rawSource) {
+		super();
+		this.name = name;
+		this.content = content;
+		this.rawSource = rawSource;
 	}
 
 	public String getContent() {
@@ -19,5 +27,9 @@ public class RawTextDocumentImpl implements RawTextDocument {
 	public String getName() {
 		return name;
 	}
-	
+
+	public String getRawSource() {
+		return rawSource;
+	}
+
 }
