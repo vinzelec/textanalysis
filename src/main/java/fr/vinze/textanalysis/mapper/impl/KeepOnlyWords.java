30b17f597e9af4a31fd77fdc7b137000a36689ee
vinzelec <leclercq.vincent@gmail.com>
2014-10-09 19:23:00 +0200

    implemented a "keep only words" mapper and set up skeleton for its test case.

---
+++ src/main/java/fr/vinze/textanalysis/mapper/impl/KeepOnlyWords.java
@@ -0,0 +1,21 @@
+package fr.vinze.textanalysis.mapper.impl;
+
+import fr.vinze.textanalysis.document.SegmentedTextDocument;
+import fr.vinze.textanalysis.document.Token;
+import fr.vinze.textanalysis.document.Word;
+import fr.vinze.textanalysis.document.impl.SegmentedTextDocumentImpl;
+import fr.vinze.textanalysis.mapper.SegmentedTextMapper;
+
+public class KeepOnlyWords implements SegmentedTextMapper {
+
+	public SegmentedTextDocument map(SegmentedTextDocument document) {
+		SegmentedTextDocument newSegmentedDoc = new SegmentedTextDocumentImpl(document.getName(), document.getSource());
+		for (Token token : document.getTokens()) {
+			if (token instanceof Word) {
+				newSegmentedDoc.getTokens().add(token);
+			}
+		}
+		return newSegmentedDoc;
+	}
+
+}
---
+++ src/test/java/fr/vinze/textanalysis/mapper/KeepOnlyWordsTest.java
@@ -0,0 +1,22 @@
+package fr.vinze.textanalysis.mapper;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class KeepOnlyWordsTest extends TestCase {
+
+	@Override
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		// TODO
+	}
+
+	@Test
+	public void testMapper() throws Exception {
+		// TODO
+	}
+
+}
