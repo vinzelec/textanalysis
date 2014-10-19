8988e22d1eebb1dcaaedb1b11a29fa8d74ea7ed7
vinzelec <leclercq.vincent@gmail.com>
2014-10-19 18:24:19 +0200

    added test case for the RecordingFilterInputStream

---
+++ src/test/java/fr/vinze/utils/RecordingFilterInputStreamTest.java
@@ -0,0 +1,80 @@
+package fr.vinze.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecordingFilterInputStreamTest {
+
+	String inputData = "This is the input data content.";
+	
+	ByteArrayInputStream input = null;
+	RecordingFilterInputStream recordingIS = null;
+
+	@Before
+	public void initStreams() {
+		input = new ByteArrayInputStream(inputData.getBytes());
+		recordingIS = new RecordingFilterInputStream(input);
+	}
+
+	@Test
+	public void testRead() throws Exception {
+		try {
+			while (recordingIS.read() != -1) {
+				// do nothing just read
+			}
+			String recorded = recordingIS.getRecorded();
+			assertEquals("equals", inputData, recorded);
+		} finally {
+			if (input != null) {
+				IOUtils.closeQuietly(input);
+			}
+			if (recordingIS != null) {
+				IOUtils.closeQuietly(recordingIS);
+			}
+		}
+	}
+
+	@Test
+	public void testReadBytes() throws Exception {
+		byte[] b = new byte[16];
+		try {
+			while (recordingIS.read(b) != -1) {
+				// do nothing just read
+			}
+			String recorded = recordingIS.getRecorded();
+			assertEquals("equals", inputData, recorded);
+		} finally {
+			if (input != null) {
+				IOUtils.closeQuietly(input);
+			}
+			if (recordingIS != null) {
+				IOUtils.closeQuietly(recordingIS);
+			}
+		}
+	}
+
+	@Test
+	public void testReadBytes2() throws Exception {
+		byte[] b = new byte[16];
+		try {
+			while (recordingIS.read(b, 0, 16) != -1) {
+				// do nothing just read
+			}
+			String recorded = recordingIS.getRecorded();
+			assertEquals("equals", inputData, recorded);
+		} finally {
+			if (input != null) {
+				IOUtils.closeQuietly(input);
+			}
+			if (recordingIS != null) {
+				IOUtils.closeQuietly(recordingIS);
+			}
+		}
+	}
+
+}
