4856002abc42df17fa54d277832263cb7ee30724
vinzelec <leclercq.vincent@gmail.com>
2014-10-19 17:44:36 +0200

    XHTMLParser implementation

--- src/main/java/fr/vinze/textanalysis/parser/impl/XHTMLParser.java
+++ src/main/java/fr/vinze/textanalysis/parser/impl/XHTMLParser.java
@@ -1,19 +1,26 @@
 package fr.vinze.textanalysis.parser.impl;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.io.IOUtils;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import fr.vinze.textanalysis.document.RawTextDocument;
+import fr.vinze.textanalysis.document.impl.RawTextDocumentImpl;
 import fr.vinze.textanalysis.parser.DocumentParser;
 import fr.vinze.textanalysis.parser.DocumentType;
 import fr.vinze.textanalysis.parser.ParseException;
+import fr.vinze.utils.RecordingFilterInputStream;
 
 public class XHTMLParser implements DocumentParser {
 
@@ -51,8 +58,73 @@ public class XHTMLParser implements DocumentParser {
 
 	protected RawTextDocument parse(File file, SAXParser parser) throws FileNotFoundException, ParseException,
 			IOException {
-		// TODO create handler for the parser then parse
-		return null;
+		InputStream input = null;
+		try {
+			input = new FileInputStream(file);
+			if (extractSource) {
+				input = new RecordingFilterInputStream(input);
+			}
+			XHTMLHandler handler = new XHTMLHandler();
+			parser.parse(input, handler);
+			RawTextDocument doc;
+			if (input instanceof RecordingFilterInputStream) {
+				doc = new RawTextDocumentImpl("", handler.getContent(),
+						((RecordingFilterInputStream) input).getRecorded());
+			} else {
+				doc = new RawTextDocumentImpl("", handler.getContent());
+			}
+			return doc;
+		} catch (SAXException e) {
+			throw new ParseException(e);
+		} finally {
+			if (input != null) {
+				IOUtils.closeQuietly(input);
+			}
+		}
+	}
+
+	private static class XHTMLHandler extends DefaultHandler {
+
+		StringBuilder builder;
+
+		public XHTMLHandler() {
+			builder = new StringBuilder();
+		}
+
+		public String getContent() {
+			return builder.toString();
+		}
+
+		boolean inBody = false;
+
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+			// at body start recording
+			if ("body".equals(localName)) {
+				inBody = true;
+			}
+			// new line
+			if ("br".equals(localName)) {
+				builder.append('\n');
+			}
+		}
+
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			// end of paragraph
+			if ("p".equals(localName)) {
+				builder.append('\n');
+			}
+		}
+
+		@Override
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			// text content
+			if (inBody) {
+				builder.append(ch);
+			}
+		}
+
 	}
 
 }
