2f636c3cab18961fbe42eb1dcca56843cc67adad
vinzelec <leclercq.vincent@gmail.com>
2014-10-20 22:54:52 +0200

    small test case for StopWords resource managment

---
+++ src/test/java/fr/vinze/textanalysis/resources/StopWordsTest.java
@@ -0,0 +1,20 @@
+package fr.vinze.textanalysis.resources;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class StopWordsTest {
+
+	@Test
+	public void testStopWordsResources() throws Exception {
+		Collection<File> files = StopWords.getInstance().getStopWords("en");
+		assertEquals("6 stop words files for english", 6, files.size());
+		files = StopWords.getInstance().getStopWords("fr");
+		assertEquals("2 stop words files for french", 2, files.size());
+	}
+
+}
