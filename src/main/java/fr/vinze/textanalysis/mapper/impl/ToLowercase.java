86c1ee301f83b00a2f3556b1bc2fe83d7f6bfc1a
vinzelec <leclercq.vincent@gmail.com>
2014-10-22 19:59:57 +0200

    ToLowercase now also mapper for RawTextDocument

--- src/main/java/fr/vinze/textanalysis/mapper/impl/ToLowercase.java
+++ src/main/java/fr/vinze/textanalysis/mapper/impl/ToLowercase.java
@@ -1,13 +1,16 @@
 package fr.vinze.textanalysis.mapper.impl;
 
+import fr.vinze.textanalysis.document.RawTextDocument;
 import fr.vinze.textanalysis.document.SegmentedTextDocument;
 import fr.vinze.textanalysis.document.Token;
 import fr.vinze.textanalysis.document.Word;
+import fr.vinze.textanalysis.document.impl.RawTextDocumentImpl;
 import fr.vinze.textanalysis.document.impl.SegmentedTextDocumentImpl;
 import fr.vinze.textanalysis.document.impl.WordImpl;
+import fr.vinze.textanalysis.mapper.RawTextMapper;
 import fr.vinze.textanalysis.mapper.SegmentedTextMapper;
 
-public class ToLowercase implements SegmentedTextMapper {
+public class ToLowercase implements SegmentedTextMapper, RawTextMapper {
 
 	public SegmentedTextDocument map(SegmentedTextDocument document) {
 		SegmentedTextDocument newSegmentedDoc = new SegmentedTextDocumentImpl(document.getName(), document.getSource());
@@ -23,6 +26,10 @@ public class ToLowercase implements SegmentedTextMapper {
 			newSegmentedDoc.getTokens().add(newToken);
 		}
 		return newSegmentedDoc;
+	}
+
+	public RawTextDocument map(RawTextDocument document) {
+		return new RawTextDocumentImpl(document.getName(), document.getContent().toLowerCase(), document.getRawSource());
 	}
 
 }
