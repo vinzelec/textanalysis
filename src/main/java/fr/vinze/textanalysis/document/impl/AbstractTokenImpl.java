9f5a912ec0a368925cff16bdaf8e85608e976b5b
vinzelec <leclercq.vincent@gmail.com>
2014-10-25 15:36:38 +0200

    mergeMetadata implementation

--- src/main/java/fr/vinze/textanalysis/document/impl/AbstractTokenImpl.java
+++ src/main/java/fr/vinze/textanalysis/document/impl/AbstractTokenImpl.java
@@ -4,9 +4,16 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import fr.vinze.textanalysis.document.Token;
 
 public abstract class AbstractTokenImpl implements Token {
+
+	private static final Logger log = LoggerFactory.getLogger(AbstractTokenImpl.class);
 
 	Map<String, Metadata<?>> metadatas;
 
@@ -35,8 +42,29 @@ public abstract class AbstractTokenImpl implements Token {
 		mergeMetadata(from, MergePolicy.IGNORE);
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void mergeMetadata(Token from, MergePolicy policy) {
-		// TODO mergeMetadata
+		for (Metadata<?> mdFrom : from.getMetadatas()) {
+			Metadata<?> mdTo = this.getMetadata(mdFrom.getKey());
+			if (mdTo == null) {
+				// don't have that metadata : copy it
+				addMetadata(mdFrom.clone());
+			} else if ((mdTo.getClass() == mdFrom.getClass()) && (mdFrom instanceof MergeableMetadata)) {
+				try {
+					((MergeableMetadata) mdTo).merge((MergeableMetadata) mdFrom);
+				} catch (OperationNotSupportedException e) {
+					log.debug("failed to merge two metadata for key " + mdTo.getKey(), e);
+					// back to MergePolicy
+					if (policy == MergePolicy.OVERRIDE) {
+						// replace original by a copy of input
+						addMetadata(mdFrom.clone());
+					}
+				}
+			} else if (policy == MergePolicy.OVERRIDE) {
+				// replace original by a copy of input
+				addMetadata(mdFrom.clone());
+			}
+		}
 
 	}
 
