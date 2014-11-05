678ea187b99b61f0196aa4ecc559999327fd77e7
vinzelec <leclercq.vincent@gmail.com>
2014-11-05 14:59:25 +0100

    svd test case completed for now

--- src/test/java/fr/vinze/textanalysis/statistics/svd/SingularValueDecompositionTest.java
+++ src/test/java/fr/vinze/textanalysis/statistics/svd/SingularValueDecompositionTest.java
@@ -28,14 +28,18 @@ public class SingularValueDecompositionTest {
 		svdBuilder = new ColtSVDBuilderImpl();
 	}
 
-	// timeout * 10
 	@Test(timeout = 1000)
 	public void testSVD() throws Exception {
 		assertTrue(svdBuilder.accept(inputMatrix));
 		SingularValueDecomposition svd = svdBuilder.decompose(inputMatrix);
 		assertNotNull(svd);
 		assertEquals("the SVD should have a rank of 9 (min of documents*terms)", 9, svd.getRank());
-		// TODO test SVD
+		// SVD can be an approximation, using 0.1 as delta between result and expected
+		double[] expectedSV = new double[] { 1.1, 0.96, 0.86, 0.76, 0.66, 0.47, 0.27, 0.17, 0.07 };
+		for (int i = 0; i < 9; i++) {
+			assertEquals("singular value at i=" + i, expectedSV[i], svd.getS()[i], 0.1);
+		}
+		// U and V matrix will depend on the terms and document orders that can't be assumed here actually
 	}
 
 }
