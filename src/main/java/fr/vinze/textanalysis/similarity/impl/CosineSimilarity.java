66cbbf4256d26ad9e5a1bb6fc0983f9ae3ac886d
vinzelec <leclercq.vincent@gmail.com>
2014-11-08 18:10:55 +0100

    Cosine similarity implementation

--- src/main/java/fr/vinze/textanalysis/similarity/impl/CosineSimilarity.java
+++ src/main/java/fr/vinze/textanalysis/similarity/impl/CosineSimilarity.java
@@ -1,12 +1,21 @@
 package fr.vinze.textanalysis.similarity.impl;
 
+import org.apache.commons.lang3.mutable.MutableDouble;
 
 public class CosineSimilarity extends AbstractVectorSimilarityDistance {
 
 	@Override
-	protected double computeDistance(double[] vector1, double[] vector2) {
-		// TODO Auto-generated method stub
-		return 0;
+	protected double computeDistance(double[] vectorA, double[] vectorB) {
+		MutableDouble dotProduct = new MutableDouble();
+		MutableDouble aSquareSum = new MutableDouble();
+		MutableDouble bSquareSum = new MutableDouble();
+		for (int i = 0; i < vectorA.length; i++) {
+			dotProduct.add(vectorA[i] * vectorB[i]);
+			aSquareSum.add(vectorA[i] * vectorA[i]);
+			bSquareSum.add(vectorB[i] * vectorB[i]);
+		}
+		return dotProduct.doubleValue()
+				/ (Math.sqrt(aSquareSum.doubleValue()) * Math.sqrt(bSquareSum.doubleValue()));
 	}
 
 }
