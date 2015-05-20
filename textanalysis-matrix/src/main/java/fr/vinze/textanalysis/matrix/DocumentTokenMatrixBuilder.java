dc05f6d667384b3031ee1ee6f9e70106cdff2a42
vinzelec <leclercq.vincent@gmail.com>
2015-05-20 15:18:22 +0200

    debug on document token matrix builder (declaration compiled without error in eclipse but failing within maven build)

--- textanalysis-matrix/src/main/java/fr/vinze/textanalysis/matrix/DocumentTokenMatrixBuilder.java
+++ textanalysis-matrix/src/main/java/fr/vinze/textanalysis/matrix/DocumentTokenMatrixBuilder.java
@@ -6,7 +6,13 @@ import java.util.Map;
 import fr.vinze.textanalysis.corpus.SegmentedTextDocumentCorpus;
 import fr.vinze.textanalysis.document.Token;
 
-public interface DocumentTokenMatrixBuilder<V, T extends DocumentTokenMatrix<? extends V>> {
+/**
+ * @param <T>
+ *            the type of matrix
+ * @param <V>
+ *            the type of the matrix values
+ */
+public interface DocumentTokenMatrixBuilder<V extends Number, T extends DocumentTokenMatrix<V>> {
 
 	T computeMatrix(SegmentedTextDocumentCorpus inputDocuments);
 
