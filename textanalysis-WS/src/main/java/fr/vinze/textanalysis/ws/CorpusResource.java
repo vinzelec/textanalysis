17a99ef585a09828b3443e93a8daad3f42d73e04
vinzelec <leclercq.vincent@gmail.com>
2015-05-22 10:19:02 +0200

    first draft of WS resources

---
+++ textanalysis-WS/src/main/java/fr/vinze/textanalysis/ws/CorpusResource.java
@@ -0,0 +1,8 @@
+package fr.vinze.textanalysis.ws;
+
+import javax.ws.rs.Path;
+
+@Path("/corpus")
+public class CorpusResource {
+
+}
---
+++ textanalysis-WS/src/main/java/fr/vinze/textanalysis/ws/DocumentResource.java
@@ -0,0 +1,10 @@
+package fr.vinze.textanalysis.ws;
+
+import javax.ws.rs.Path;
+
+@Path("/corpus/{cid}/document")
+public class DocumentResource {
+
+	// TODO
+
+}
